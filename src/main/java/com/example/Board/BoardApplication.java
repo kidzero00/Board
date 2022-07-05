7cd569f99de4ec64e96b87e7f1ab24f962a284df
kidzero00 <dkdud203@naver.com>
2022-07-05 11:25:08 +0900

    Initial commit

---
+++ src/main/java/com/example/Board/BoardApplication.java
@@ -0,0 +1,14 @@
+package com.example.Board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardApplication.class, args);
+		System.out.println("hello project!");
+	}
+
+}
